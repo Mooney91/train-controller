47719492327cbf9317053af258179344fe33dbda
Zachary Mooney <zamo22@student.bth.se>
2023-09-14 21:21:21 +0200

    Implement Vue.js

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <TrafficController msg="Välkommen till Traffic Controller"/>
+</template>
+
+<script>
+import TrafficController from './components/TrafficController.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TrafficController
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/TrafficController.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container" id="container"></div>
+</template>
+
+<script>
+export default {
+  name: 'TrafficController',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
