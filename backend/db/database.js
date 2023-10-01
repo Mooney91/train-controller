7e36f882273cb4161f24cfd3862ed2b586ffcb1b
Jonas B <117373198+jonasbth@users.noreply.github.com>
2023-10-01 08:36:34 +0200

    Test with deployed MongoDB.

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,7 +1,11 @@
 const sqlite3 = require("sqlite3").verbose();
 const { open } = require("sqlite");
 const { MongoClient } = require("mongodb");
-const dsnMongo = "mongodb://localhost:27017";
+//const dsnMongo = "mongodb://localhost:27017";
+//const dsnMongo = "mongodb+srv://jsramverk:KXzO5CuPQA@mycluster.fmupkp8.mongodb.net/" +
+//                 "?retryWrites=true&w=majority";
+const dsnMongo = "mongodb+srv://trainController:jsramverkTrains2023@cluster0.djy5hpi.mongodb.net/" +
+                 "?retryWrites=true&w=majority";
 let dbName = "trains";
 
 if (process.env.NODE_ENV === 'test') {
